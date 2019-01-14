28a41c9fc8ae13c26aa8d3219906edc509fba06c
rv98931n@pace.edu <rahultrail>
2019-01-14 13:25:07 +0000

    colab -> github

---
+++ nmp/index.js
@@ -0,0 +1,164 @@
+
+const HOSTED_URLS = {
+  model:
+      'model_js/model.json',
+  metadata:
+      'model_js/metadata.json'
+};
+
+const examples = {
+  'example':
+      'British actress Emilia Clarke was born in London and grew up in Berkshire, England. 
+        Her father is a theatre sound engineer and her mother is a businesswoman. Her father was working on a theatre production of "Show Boat" and her mother took her along to the performance. This is when, at the age of 3, her passion for drama began. 
+        From 2000 to 2005, she attended St. Edward's School of Oxford, where she appeared in two school plays. She went on to study at the prestigious Drama Centre London, where she took part in 10 plays. 
+        During this time, Emilia first appeared on television with a guest role in the BBC soap opera Doctors (2000).
+        In 2010, after graduating from the Drama Centre London, Emilia got her first film role in the television movie Triassic Attack (2010). 
+        In 2011, her breakthrough role came in when she replaced fellow newcomer Tamzin Merchant on Game of Thrones (2011) after the filming of the initial pilot. 
+        Emilia won the 2011 EWwy Award for Best Supporting Actress in a Drama for her role as Daenerys Targaryen. 
+        From March to April 2013, she played Holly Golightly in a Broadway production of "Breakfast at Tiffany's". 
+        She played Sarah Connor in Terminator Genisys (2015), opposite Arnold Schwarzenegger, Jai Courtney and Jason Clarke. '
+        };
+
+function status(statusText) {
+  console.log(statusText);
+  document.getElementById('status').textContent = statusText;
+}
+
+function showMetadata(metadataJSON) {
+  document.getElementById('vocabularySize').textContent =
+      metadataJSON['vocabulary_size'];
+  document.getElementById('maxLen').textContent =
+      metadataJSON['max_len'];
+}
+
+function settextField(text, predict) {
+  const textField = document.getElementById('text-entry');
+  textField.value = text;
+  doPredict(predict);
+}
+
+function setPredictFunction(predict) {
+  const textField = document.getElementById('text-entry');
+  textField.addEventListener('input', () => doPredict(predict));
+}
+
+function disableLoadModelButtons() {
+  document.getElementById('load-model').style.display = 'none';
+}
+
+function doPredict(predict) {
+  const textField = document.getElementById('text-entry');
+  const result = predict(textField.value);
+  score_string = "Class scores: ";
+  for (var x in result.score) {
+    score_string += x + " ->  " + result.score[x].toFixed(3) + ", "
+  }
+  //console.log(score_string);
+  status(
+      score_string + ' elapsed: ' + result.elapsed.toFixed(3) + ' ms)');
+}
+
+function prepUI(predict) {
+  setPredictFunction(predict);
+  const testExampleSelect = document.getElementById('example-select');
+  testExampleSelect.addEventListener('change', () => {
+    settextField(examples[testExampleSelect.value], predict);
+  });
+  settextField(examples['example1'], predict);
+}
+
+async function urlExists(url) {
+  status('Testing url ' + url);
+  try {
+    const response = await fetch(url, {method: 'HEAD'});
+    return response.ok;
+  } catch (err) {
+    return false;
+  }
+}
+
+async function loadHostedPretrainedModel(url) {
+  status('Loading pretrained model from ' + url);
+  try {
+    const model = await tf.loadModel(url);
+    status('Done loading pretrained model.');
+    disableLoadModelButtons();
+    return model;
+  } catch (err) {
+    console.error(err);
+    status('Loading pretrained model failed.');
+  }
+}
+
+async function loadHostedMetadata(url) {
+  status('Loading metadata from ' + url);
+  try {
+    const metadataJson = await fetch(url);
+    const metadata = await metadataJson.json();
+    status('Done loading metadata.');
+    return metadata;
+  } catch (err) {
+    console.error(err);
+    status('Loading metadata failed.');
+  }
+}
+
+class Classifier {
+
+  async init(urls) {
+    this.urls = urls;
+    this.model = await loadHostedPretrainedModel(urls.model);
+    await this.loadMetadata();
+    return this;
+  }
+
+  async loadMetadata() {
+    const metadata =
+        await loadHostedMetadata(this.urls.metadata);
+    showMetadata(metadata);
+    this.maxLen = metadata['max_len'];
+    console.log('maxLen = ' + this.maxLen);
+    this.wordIndex = metadata['word_index']
+  }
+
+  predict(text) {
+    // Convert to lower case and remove all punctuations.
+    const inputText =
+        text.trim().toLowerCase().replace(/(\.|\,|\!)/g, '').split(' ');
+    // Look up word indices.
+    const inputBuffer = tf.buffer([1, this.maxLen], 'float32');
+    for (let i = 0; i < inputText.length; ++i) {
+      const word = inputText[i];
+      inputBuffer.set(this.wordIndex[word], 0, i);
+      //console.log(word, this.wordIndex[word], inputBuffer);
+    }
+    const input = inputBuffer.toTensor();
+    //console.log(input);
+
+    status('Running inference');
+    const beginMs = performance.now();
+    const predictOut = this.model.predict(input);
+    //console.log(predictOut.dataSync());
+    const score = predictOut.dataSync();//[0];
+    predictOut.dispose();
+    const endMs = performance.now();
+
+    return {score: score, elapsed: (endMs - beginMs)};
+  }
+};
+
+async function setup() {
+  if (await urlExists(HOSTED_URLS.model)) {
+    status('Model available: ' + HOSTED_URLS.model);
+    const button = document.getElementById('load-model');
+    button.addEventListener('click', async () => {
+      const predictor = await new Classifier().init(HOSTED_URLS);
+      prepUI(x => predictor.predict(x));
+    });
+    button.style.display = 'inline-block';
+  }
+
+  status('Standing by.');
+}
+
+setup();
